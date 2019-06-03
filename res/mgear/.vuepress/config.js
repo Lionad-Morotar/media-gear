f8352825654d81aae7e0a13a0c781a14dfb7c3e1
lionad <tangnad@qq.com>
2019-06-03 18:15:54 +0800

    feat: cedium.css

---
+++ res/mgear/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'MGear',
+  description: '媒体链路博客, 前端链接世界',
+  themeConfig: {
+    searchMaxSuggestions: 10,
+    logo: './dist/assets/img/favicon.svg',
+    nav: [
+      { text: '网易云', link: 'https://music.163.com/#/user/home?id=64236446' },
+      { text: '掘金', link: 'juejin.im/user/5b209f666fb9a01e66165c5a' },
+      { text: 'Github', link: 'https://github.com/Lionad-Morotar' }
+    ],
+    sidebar: 'auto',
+    lastUpdated: 'Last Updated'
+  }
+}
